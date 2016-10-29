d5fbc863f77ee43a78997f5b71ee8421cc8d25cb
ravend6 <ravend6@gmail.com>
2016-10-29 18:29:43 +0300

    Fix more

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -24,8 +24,10 @@ export default class Nav extends React.Component {
         <div className="top-bar-left">
           <ul className="menu">
             <li className="menu-text">React Weather App</li>
-            <li><IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink></li>
-            <li><Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link></li>
+            <li><IndexLink to="/" activeClassName="active"
+              activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink></li>
+            <li><Link to="/about" activeClassName="active"
+              activeStyle={{fontWeight: 'bold'}}>About</Link></li>
           </ul>
         </div>
         <div className="top-bar-right">
